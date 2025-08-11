26f8d2f2f829383589482e986b5369909770a86d
Ayush Mittal <mittalayush2003@gmail.com>
2025-08-11 12:57:19 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import LoginButtons from '../components/LoginButtons'
 
 const Home = () => {
   const handleGithubLogin = () => {
-    const clientId = "";
+    const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
     const redirectUri = encodeURIComponent("https://crispy-waffle-qrvgxpgj5w4hxv7r-5000.app.github.dev/github/auth/github/callback");
     const scope = "read:user user:email";
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&login`;
@@ -10,7 +10,7 @@ const Home = () => {
   }
 
   const handleGitlabLogin = () => {
-    const clientId = "22d1b46af9ef1be2b4d3d85a7d57b1821514db93252e5429bb2448c704f25853";
+    const clientId =  import.meta.env.VITE_GITLAB_CLIENT_ID;
     const redirectUri = encodeURIComponent("https://crispy-waffle-qrvgxpgj5w4hxv7r-5000.app.github.dev/gitlab/auth/gitlab/callback");
     const scope = encodeURIComponent("read_user api");
     const gitlabAuthUrl = `https://gitlab.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&prompt=login`;
